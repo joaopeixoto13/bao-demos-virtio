3e9cdb2347de69f282c1384fcd5c67aa683a1b16
joaopeixoto13 <joaopeixotooficial@gmail.com>
2024-04-08 17:19:07 +0100

    fix(demo-4): bring down eth0 before creating the bridge
    This patch brings down the eth0 interface before creating and adding
    the same interface to bao's bridge, preventing the Backend VM network
    interface from being unreachable by the other devices connected to the
    same network. Also, the TAPs devices are bring it up after added to the
    bridge.
    
    Signed-off-by: joaopeixoto13 <joaopeixotooficial@gmail.com>

--- demo-4/virtio_net_setup.sh
+++ demo-4/virtio_net_setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+# Down eth0
+ifconfig eth0 down
+
 # Create tap0 and tap1 devices
 ip tuntap add dev tap0 mode tap
 ip tuntap add dev tap1 mode tap
-
-# Configure tap0 and tap1 devices
-ifconfig tap0 192.168.42.14 up
-ifconfig tap1 192.168.42.15 up
 
 # Create bridge bao-br0
 brctl addbr bao-br0
@@ -19,9 +18,16 @@ brctl addif bao-br0 eth0
 brctl addif bao-br0 tap0
 brctl addif bao-br0 tap1
 
-# Launch 2 Bao VirtIO Device Models
+# Bring up eth0
+ifconfig eth0 up
+
+# Configure tap0 and tap1 devices and bring it up
+ifconfig tap0 192.168.42.14 up
+ifconfig tap1 192.168.42.15 up
+
+# Launch 2 Bao VirtIO Device Models to serve the two Frontend's
 nohup bao-virtio --config ~/config-virtio-vm1.yaml > ~/bao-virtio.log 2>&1 &
 nohup bao-virtio --config ~/config-virtio-vm2.yaml > ~/bao-virtio.log 2>&1 &
 
-# Launch only 1 Bao VirtIO Device Model
+# Launch only 1 Bao VirtIO Device Model to serve the two Frontend's
 #nohup bao-virtio --config ~/config-virtio-vms.yaml > ~/bao-virtio.log 2>&1 &
